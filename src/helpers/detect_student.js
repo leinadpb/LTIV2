719939efc682c8cf660c007ba9b4e88b12927884
Daniel Pena Berroa <digrape07@gmail.com>
2019-06-24 18:25:16 -0400

    AD user and passwrod

--- src/helpers/detect_student.js
+++ src/helpers/detect_student.js
@@ -2,9 +2,10 @@ const ActiveDirectory = require('activedirectory');
 
 require('dotenv').config();
 
-console.log('Credentials for AD are: ', process.env.AD_USER, process.env.AD_PASSWORD);
+const CURRENT_LOGGED_IN_USER = process.env.AD_USER.substr(0, process.env.AD_USER.indexOf('@'));
+console.log('username: ', CURRENT_LOGGED_IN_USER);
 
-const CURRENT_LOGGED_IN_USER = '1066359@intec';
+console.log('Credentials for AD are: ', process.env.AD_USER, process.env.AD_PASSWORD);
 
 const config = { url: 'ldap://lab.intec',
                baseDN: 'dc=lab,dc=intec',
