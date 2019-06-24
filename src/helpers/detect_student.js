580643620fc715691ae167156d012259b542a2dd
Daniel Pena Berroa <digrape07@gmail.com>
2019-06-24 17:53:59 -0400

    dotenv

--- src/helpers/detect_student.js
+++ src/helpers/detect_student.js
@@ -1,11 +1,15 @@
 const ActiveDirectory = require('activedirectory');
 
+require('dotenv').config();
+
+console.log(process.env.ANGELO_PASSWORD);
+
 const CURRENT_LOGGED_IN_USER = '1066359';
 
-const config = { url: 'ldap://dc.domain.com',
-               baseDN: 'dc=domain,dc=com',
-               username: 'username@domain.com',
-               password: 'password' }
+const config = { url: 'lab.intec',
+               baseDN: 'dc=lab,dc=intec',
+               username: 'angelo.paredes@intecadm',
+               password: process.env.ANGELO_PASSWORD }
 
 const ad = new ActiveDirectory(config);
 ad.findUser(CURRENT_LOGGED_IN_USER, function(err, user) {
