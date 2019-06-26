d86f488a0eadffab4f91d81035c80407ad3c11e5
Daniel Pena Berroa <digrape07@gmail.com>
2019-06-25 22:57:06 -0400

    app functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const killBrowsers = require('./helpers/kill_browsers');
 const test = require('./helpers/test_helper');
 const settings = require('./settings');
@@ -24,10 +24,39 @@ const executeJobs = () => {
 }
 
 const showSurvey = (url) => {
-  console.log('Show survey: ', url);
+  window = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    alwaysOnTop: true,
+    frame: false,
+    fullscreen: true
+  });
+  window.loadFile(path.join(__dirname, 'pages', `${settings.PAGES.surveyPage}.html`));
+  window.webContents.openDevTools();
+  window.on('close', () => {
+    console.log('You have closed the window');
+    app.quit();
+  })
 }
 
-const showReminder = () => {
+const showSurveyOrClose = (APP_PREFERENCES) => {
+  // TODO: Move this code to execute AFTER ONE OF
+  // THE PREVIOUS WINDOWAS ARE CLOSED....
+  if (APP_PREFERENCES.showSurvey) {
+    if (userDomain === "intec") {
+      showSurvey(APP_PREFERENCES.studentUrl);
+    } else {
+      showSurvey(APP_PREFERENCES.teacherUrl);
+    }
+  } else {
+    app.quit();
+  }
+}
+
+const showReminder = (APP_PREFERENCES) => {
   window = new BrowserWindow({
     width: 1100,
     height: 500,
@@ -35,80 +64,88 @@ const showReminder = () => {
       nodeIntegration: true
     },
     alwaysOnTop: true,
-    frame: true
+    frame: false,
+    resizable: false
   });
   window.loadFile(path.join(__dirname, 'pages', `${settings.PAGES.reminderPage}.html`));
+  window.webContents.openDevTools();
   window.on('close', () => {
-    console.log('You have closed the window');
+    console.log('You have closed reminder window');
+    showSurveyOrClose(APP_PREFERENCES);
   })
 }
 
-const showRules = (username, trimester) => {
-  console.log('Will show rules to user');
+const showRules = async (username, trimester, APP_PREFERENCES) => {
+  const RULES = await queries.getRules();
+  window = new BrowserWindow({
+    width: 1100,
+    height: 500,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    alwaysOnTop: true,
+    frame: false,
+    resizable: false,
+    fullscreen: true
+  });
+  window.labRules = RULES;
+  window.loadFile(path.join(__dirname, 'pages', `${settings.PAGES.rulesPage}.html`));
+  window.webContents.openDevTools();
+  window.on('close', () => {
+    console.log('You have closed rules window');
+    showSurveyOrClose(APP_PREFERENCES);
+  })
 }
 
 app.on('ready', async () => {
 
-  // For angelo rapid developemnt
-  showReminder();
+  // connect to DB
+  require('./helpers/connect_db')
+  // Seed data if needed
+  require('./db/seed');
+  // get configs
+  const configs = await queries.getConfigs();
+  // get trimestres
+  const trimesters = await queries.getTrimesters();
+  // get user info
+  let userDomain = process.env.USERDOMAIN || "intec";
+  let userName = process.env.USERNAME || os.userInfo().username || "1066359"; // TEST
 
-  // // connect to DB
-  // require('./helpers/connect_db')
-  // // Seed data if needed
-  // require('./db/seed');
-  // // get configs
-  // const configs = await queries.getConfigs();
-  // // get trimestres
-  // const trimesters = await queries.getTrimesters();
-  // // get user info
-  // let userDomain = process.env.USERDOMAIN || "intec";
-  // let userName = process.env.USERNAME || os.userInfo().username || "1066359"; // TEST
+  const currentTrimester = await queries.getCurrentTrimester();
 
-  // const currentTrimester = await queries.getCurrentTrimester();
+  const APP_PREFERENCES = {
+    fullscreen: configs.find(cfg => cfg.key === settings.CONFIGS.isFullscreen).value,
+    showSurvey: configs.find(cfg => cfg.key === settings.CONFIGS.showSurvey).value,
+    studentUrl: configs.find(cfg => cfg.key === settings.CONFIGS.studentUrl).value,
+    teacherUrl: configs.find(cfg => cfg.key === settings.CONFIGS.teacherUrl).value,
+  }
 
-  // const APP_PREFERENCES = {
-  //   fullscreen: configs.find(cfg => cfg.key === settings.CONFIGS.isFullscreen).value,
-  //   showSurvey: configs.find(cfg => cfg.key === settings.CONFIGS.showSurvey).value,
-  //   studentUrl: configs.find(cfg => cfg.key === settings.CONFIGS.studentUrl).value,
-  //   teacherUrl: configs.find(cfg => cfg.key === settings.CONFIGS.teacherUrl).value,
-  // }
+  const STUDENTS = await queries.getStudentInCurrentTrimester(currentTrimester[0]);
+  const CURRENT_STUDENT = STUDENTS[0];
 
-  // const STUDENTS = await queries.getStudentInCurrentTrimester(currentTrimester[0]);
-  // const CURRENT_STUDENT = STUDENTS[0];
-
-  // if (!CURRENT_STUDENT) {
-  //   console.log(CURRENT_STUDENT);
-  //   await queries.addStudent({
-  //     name: userName,
-  //     intecId: userName,
-  //     fullName: userName,
-  //     computer: os.hostname(),
-  //     room: '',
-  //     createdAt: Date.now(),
-  //     subject: '',
-  //     trimesterName: currentTrimester[0].name,
-  //     domain: userDomain
-  //   });
-  //   showRules(userName, currentTrimester[0]);
-  // } else {
-  //   showReminder();
-  // }
-
-  // TODO: Move this code to execute AFTER ONE OF
-  // THE PREVIOUS WINDOWAS ARE CLOSED....
-  // if (APP_PREFERENCES.showSurvey) {
-  //   if (userDomain === "intec") {
-  //     showSurvey(APP_PREFERENCES.studentUrl);
-  //   } else {
-  //     showSurvey(APP_PREFERENCES.teacherUrl);
-  //   }
-  // }
-
+  if (!CURRENT_STUDENT) {
+    console.log(CURRENT_STUDENT);
+    await queries.addStudent({
+      name: userName,
+      intecId: userName,
+      fullName: userName,
+      computer: os.hostname(),
+      room: '',
+      createdAt: Date.now(),
+      subject: '',
+      trimesterName: currentTrimester[0].name,
+      domain: userDomain
+    });
+    showRules(userName, currentTrimester[0], APP_PREFERENCES);
+  } else {
+    showReminder(APP_PREFERENCES);
+  }
+  
   // Execute this code to Close any browser. So user first completes this process and then,
   //  can use the computer.
-  // jobs = setInterval(() => {
-  //   executeJobs();
-  // }, 5000);
+  jobs = setInterval(() => {
+    executeJobs();
+  }, 5000);
 });
 
 app.on('window-all-closed', () => {
