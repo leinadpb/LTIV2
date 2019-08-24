45f8c090f42af092a8dc902c03c8d6442c66c64b
Daniel Pena Berroa <digrape07@gmail.com>
2019-08-24 11:09:27 -0400

    using squirrel events, electorn-packager and electorn-winstaller to generate windows installer, in order to avoid heavy app bundle sizes

--- src/helpers/generate_windows.js
+++ src/helpers/generate_windows.js
@@ -1,25 +1,31 @@
 const electronInstaller = require('electron-winstaller');
+const path = require('path');
+const log = require('electron-log');
 
 let folderName = process.platform.toLocaleLowerCase() === 'darwin' ? 'labapp-darwin-x64' : 'labapp-win32-x64';
 
 // Only windows is supported >>>>
-folderName = 'labapp-win32-x64';
-const init = async () => {
-    try {
-        await electronInstaller.createWindowsInstaller({
-          appDirectory: `./out/${folderName}`,
-          outputDirectory: './installers/windows',
-          authors: 'Daniel Pena y Angelo Paredes',
-          exe: 'labapp.exe',
-          version: '1.0.0',
-          title: 'LAB APP',
-          description: 'Lab app for rules and surveys from students and teachers.'
-        });
-        console.log('Lab app installer has been generated succesfully!');
-        console.log(`\\o/`);
-      } catch (e) {
-        console.log(`Error when generating installer: ${e.message}`);
-      }
-}
+folderName = 'labapp-win32-ia32';
 
-init();
+(async () => {
+    try {
+      await electronInstaller.createWindowsInstaller({
+        appDirectory: path.resolve(__dirname, '..', '..', 'installers', 'windows', folderName),
+        outputDirectory: path.resolve(__dirname, '..', '..', 'installers', 'windows'),
+        authors: 'Daniel Pena y Angelo Paredes',
+        owners: 'INTEC',
+        exe: 'labapp.exe',
+        version: '1.0.0',
+        title: 'LAB APP',
+        description: 'Lab app for rules and surveys from students and teachers.',
+        noMsi: true,
+        setupIcon: path.join(__dirname, '..', 'images', 'labti-logo.ico'),
+        iconUrl: path.join(__dirname, '..', 'images', 'labti-logo.ico'),
+        setupExe: 'LabAppStudents.exe',
+      });
+      log.info('Lab app installer has been generated succesfully!');
+      log.info(`\\o/`);
+    } catch (e) {
+      log.error(`Error when generating installer: ${e.message}`);
+    }
+})();
